5dfff9d4475daf1a28434f872430d3b29770ccd4
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2021-06-02 17:31:55 +0200

    working on data query

--- Dashboard/src/app/pages/start/start.component.ts
+++ Dashboard/src/app/pages/start/start.component.ts
@@ -19,20 +19,45 @@ export class StartComponent implements OnInit {
   levelvalues:any;
   subgroups:any;
   outcomes:any;
-
+  levelsettings={};
+  
   ngOnInit(): void {
 
     this.metadata = this.api.getmetadata("metadata");
     this.sortdata = this.api.getmetadata("sortdata");
     this.level = this.api.filterArray(this.metadata,"type","level")[0]["varname"];
     this.levelvalues = this.api.filterArray(this.sortdata,"varname",this.level)[0]["values"];
-    this.subgroups = this.api.getValues(this.api.filterArray(this.metadata,"type","group"),"varname");
+    if (this.levelvalues.length>0){this.setlevel("levelvalues", this.levelvalues[0])}
+    this.subgroups = ["Keine"].concat(this.api.getValues(this.api.filterArray(this.metadata,"type","group"),"varname"));
+    if (this.subgroups.length>0){this.setlevel("subgroups", this.subgroups[0])}
     this.outcomes = this.api.getValues(this.api.sortArray(this.api.filterArray(this.metadata,"topic","outcomes"),"varname"),"varname");
-    console.log(this.metadata,this.sortdata,this.level,this.levelvalues,this.subgroups,this.outcomes);
-
+    if (this.outcomes.length>0){this.setlevel("outcomes", this.outcomes[0])}
+    this.querydata();
 
   }
 
+  setlevel(level,value){
+    this.levelsettings[level]=value;    
+  }
+
+  querydata(){
+    let query = {
+      "client_id": this.api.REST_API_SERVER_CLIENTID,
+      "groupinfo": {},
+      "showfields": [this.levelsettings["outcomes"]]};
+    query["groupinfo"][this.level] = this.levelsettings["levelvalues"];
+    console.log(query);
+    for (let group in this.subgroups){
+      if (this.levelsettings["subgroups"]!=group){
+        query["groupinfo"][group]  = this.api.filterArray(this.sortdata,"varname",group)[0]["allforlevel"];
+      }
+      else {
+        delete query["groupinfo"][group];
+      };
+    }
+    console.log(query);
+    //return this.api.postTypeRequest('get_data/',query);
+  }
 
 
 }
